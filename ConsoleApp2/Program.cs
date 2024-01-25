ca3f9f76ee2066982ad7398822bcc2e24e63952e
mapclean9r <tobias.the@hotmail.no>
2024-01-25 18:54:13 +0100

    // Testa ut sofistikerte klasser

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Ware;
+
 Console.WriteLine("Hello, World!");
-Ware.Class1.Main();
+CreatePackages u = new CreatePackages("urrr", "eeee");
+u.name = "package name";
+u.packageid = "imeim2f";
+
+Console.WriteLine(u.name);
+Console.WriteLine(u.li());
