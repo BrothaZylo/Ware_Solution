6aa62501902774e213bcb12972fd4f32e02ef76b
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-01-25 17:39:40 +0100

    lagde consoleapp

--- Ware/Class1.cs
+++ Ware/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Ware
+﻿using System;
+
+namespace Ware
 {
     public class Class1
     {
-
+        public static void Main()
+        {
+            Console.WriteLine("Hello from Class1!");
+        }
     }
 }
