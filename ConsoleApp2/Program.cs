6d1b05473d1b9d633ecc496b159986a3cccf4624
mapclean9r <tobias.the@hotmail.no>
2024-01-27 19:45:55 +0100

    // added SendToWareHouse() // removed redundant idgen

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Ware;
 
-CreatePackage u = new CreatePackage("eeee");
-u.name = "package name";
+CreatePackage u = new("Hestesko", "kjølevare", "fast", 82.5, 43.4);
 
 Console.WriteLine(u.name);
 Console.WriteLine(u.packageid);
+Console.WriteLine(u.goods);
+Console.WriteLine(u.speed);
+Console.WriteLine(u.height);
+Console.WriteLine(u.width);
+
+CreatePackage xd = CreatePackage.SendToWareHouse(u);
+Console.WriteLine(xd.name+" "+xd.packageid);
--- Ware/CreatePackage.cs
+++ Ware/CreatePackage.cs
@@ -3,36 +3,35 @@ using System.Runtime.InteropServices;
 
 namespace Ware
 {
-    public class CreatePackage : IPackage
+    public class CreatePackage(string packagename, string goodstype, string speedofdelivery, double packageheightCM, double packagewidthCM) : IPackage
     {
-        public string packageid;
-        public string name;
+        public string packageid = GenerateId(), name = packagename, goods = goodstype, speed = speedofdelivery;
+        public double height = packageheightCM, width = packagewidthCM;
 
-        public CreatePackage(string name)
+        public static string GenerateId()
         {
-            name = name;
-            packageid = GenerateId();
-        }
-
-        public string GenerateId()
-        {
-            var selection = "abcdefghijklmnopqrstuvwxyz123456789";
+            var selection = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
             var newid = new char[15];
             for (int i = 0; i < 15; i++)
             {
-                Random rand = new Random();
-                int num = rand.Next(selection.Length);
-                int randomchar = selection.ElementAt(num);
+                Random rand = new();
                 newid[i] = selection[rand.Next(selection.Length)];
             }
             var trueid = new String(newid);
             return trueid;
         }
 
-        public string 
+        public static string Inte(string x)
+        {
+            return x;
+        }
 
+        public static CreatePackage SendToWareHouse(CreatePackage packet)
+        {
+            return packet;
+        }
 
-        void IPackage.GenerateId()
+        public void SendToWareHouse()
         {
             throw new NotImplementedException();
         }
