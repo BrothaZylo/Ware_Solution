ddcf78c119d205d53d6389b4c18fbb135c20f625
stian <stian@DESKTOP-1EHJ2CQ>
2024-01-28 17:23:11 +0100

    Added productHistory

---
+++ Ware/DeliveryTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    class DeliveryTime
+    {
+        public DateTime DeliverySchedule;
+        public List<CreatePackage> Packages;
+
+        public DeliveryTime(DateTime deliverySchedule, List<CreatePackage> packages)
+        {
+            DeliverySchedule = deliverySchedule;
+            Packages = packages;
+        }
+
+    }
+}
+
+
---
+++ Ware/PackageHistory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    class PackageHistory
+    {
+        private Dictionary<CreatePackage, (DateTime DeliveryTime, DateTime PickupTime)> History;
+
+        public PackageHistory(Dictionary<CreatePackage, (DateTime DeliveryTime, DateTime PickupTime)> history)
+        {
+            History = history;
+        }
+
+        public void DeliveryHistory(CreatePackage packages, DateTime deliveryTime)
+        {
+            // Creates the log for deliverytime and using DateTime.MinValue as a placeholder for PickupTime
+            History.Add(packages,(deliveryTime, DateTime.MinValue));
+        }
+
+        //Updates the tuple by not changing deliveryTime and adds pickupTime
+        public void PickTime(CreatePackage package, DateTime pickupTime)
+        {
+            if (History.ContainsKey(package))
+            {
+                var (deliveryTime, _) = History[package];
+                History[package] = (deliveryTime, pickupTime);
+            }
+        }
+       
+        public Dictionary<CreatePackage, (DateTime DeliveryTime, DateTime PickupTime)> AllHistory()
+        {
+            return new Dictionary<CreatePackage, (DateTime DeliveryTime, DateTime PickupTime)>(History);
+        }
+        public List<(CreatePackage package, DateTime deliveryTime, DateTime pickupTime)> OnePackageHistory(string packageId)
+        {
+            List<(CreatePackage package, DateTime deliveryTime, DateTime pickupTime)> packageHistory = new List<(CreatePackage package, DateTime deliveryTime, DateTime pickupTime)>();
+            foreach (var item in History)
+            {
+                CreatePackage checkPackage = item.Key;
+                DateTime delivery = item.Value.DeliveryTime;
+                DateTime pickup = item.Value.PickupTime;
+                
+                if(checkPackage.packageid == packageId)
+                {
+                    packageHistory.Add((checkPackage, delivery, pickup));
+                }
+            }
+            return packageHistory;
+           
+        }
+    }
+}
