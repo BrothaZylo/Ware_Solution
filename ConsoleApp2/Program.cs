2014e5ccf45998527f8f488d423a3e81bdd2fc91
mapclean9r <tobias.the@hotmail.no>
2024-01-29 17:53:47 +0100

    // Fixed class setup :D

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,15 +6,12 @@ CreatePackage u = new("Hestesko", "kjølevare", "fast", 82.5, 43.4);
 
 Console.WriteLine(u.name);
 Console.WriteLine(u.packageid);
-List<WareHouseSizeConfig> configlist =
+List<CreateShelves.WareHouseSizeConfig> configlist =
 [
     new() { Sizename = "Tiny", Totalunitsavailable = 10, Maxlengthcm = 10.5, Maxwidthcm = 5},
     new() { Sizename = "Large", Totalunitsavailable = 15, Maxlengthcm = 30, Maxwidthcm = 30 }
 ];
 
-ShelvesUnits house = new("Frysevarer", 25, configlist);
+CreateShelves house = new("Frysevarer", 25, configlist);
 
-foreach(WareHouseSizeConfig config in configlist)
-{
-    Console.WriteLine("StorageName: "+config.Sizename+" TotalUnits: "+config.Totalunitsavailable+" Max Length CM: "+config.Maxlengthcm+" Max Width CM: "+config.Maxwidthcm);
-}
+house.GetSizeConfig();
---
+++ Ware/CreateShelves.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    public class CreateShelves(string nameofstorage, int totalspaceavailable, List<CreateShelves.WareHouseSizeConfig> configuresize) : IWareHouse
+    {
+        public string Shelfcategory = nameofstorage;
+        public int Totalspace = totalspaceavailable;
+        public List<WareHouseSizeConfig> Configfiles = configuresize;
+
+        public void GetSizeConfig()
+        {
+            foreach (var Item in Configfiles)
+            {
+                Console.WriteLine("StorageName: " + Item.Sizename + " TotalUnits: " + Item.Totalunitsavailable + " Max Length CM: " + Item.Maxlengthcm + " Max Width CM: " + Item.Maxwidthcm);
+            }
+        }
+
+        public class WareHouseSizeConfig
+        {
+            public required string Sizename { get; set; }
+            public int Totalunitsavailable { get; set; }
+            public double Maxwidthcm { get; set; }
+            public double Maxlengthcm { get; set; }
+        }
+    }
+}
