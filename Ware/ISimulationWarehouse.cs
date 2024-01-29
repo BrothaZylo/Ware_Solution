55689c060ab716f1139ebb5c8fddb90f0175a56c
samhu-hue <55948747+samhu-hue@users.noreply.github.com>
2024-01-29 17:12:45 +0100

    added simulation interface

---
+++ Ware/ISimulationWarehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    internal interface ISimulationWarehouse
+    {
+        ///<summary>
+        ///Runs the simulation to simulate a working warehouse. The warehouse will receive packages also send out packages.
+        /// </summary>
+    }
+}
