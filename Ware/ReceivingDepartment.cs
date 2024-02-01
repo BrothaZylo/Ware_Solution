5188a1d414e7232b4dfbe44c49fcebe59b9d2f63
mapclean9r <tobias.the@hotmail.no>
2024-02-01 14:57:42 +0100

    // :D

---
+++ Ware/ReceivingDepartment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    public class ReceivingDepartment()
+    {
+        // liste med varer
+    }
+
+    // metode, en metode som sender 1 og 1 til varehuset fra listen
+    // Den skal også ha tiden det tar fra varemottaket til hyllene (gods typen/hylletypen(feks Frysevare))
+
+}
