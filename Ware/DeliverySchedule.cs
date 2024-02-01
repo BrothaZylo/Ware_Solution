22a85e476077e7b738f85cd65e3604aa14c2e565
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2024-02-01 22:56:59 +0100

    Added a way for user to add packages to a daily/weekly schedule delivery/pickup

---
+++ Ware/DeliverySchedule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ware
+{
+    public class DeliverySchedule
+    {
+        private List<(CreatePackage package, DateTime deliveryTime)>DailyPackages;
+        private List<(CreatePackage package, DateTime deliveryTime)>WeeklyPackages;
+
+
+        public DeliverySchedule() 
+        {
+            DailyPackages = new List<(CreatePackage package, DateTime deliveryTime)>();
+            WeeklyPackages = new List<(CreatePackage package, DateTime deliveryTime)>();
+        }
+        public void addDailyPackages(CreatePackage package, DateTime time)
+        {
+            DailyPackages.Add((package, time));
+        }
+        public void addWeeklyPackages(CreatePackage package, DateTime time)
+        {
+            WeeklyPackages.Add((package, time));
+        }
+
+        public List<(CreatePackage, DateTime)> getDailyPackages()
+        {
+            return DailyPackages;
+        }
+        public List<(CreatePackage, DateTime)> getWeeklyPackages() 
+        {
+            return WeeklyPackages;
+        }
+
+    }
+}
