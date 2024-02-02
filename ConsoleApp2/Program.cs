67045c6cb140d6e3555b6ac9ff33d399312ffd87
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-02-02 19:47:17 +0100

    Endra xml til engelsk og update klassen

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@ class Program
     static void Main(string[] args)
     {
         CreatePackage u = new("Hestesko", "Frysevarer", "fast", 3, 5);
+        CreatePackage uu = new("Hestesko", "Frysevarer", "fast", 3, 5);
 
         List<StorageConfiguration.WareHouseSizeConfig> configlist =
         [
@@ -20,34 +21,24 @@ class Program
         ];
 
         StorageConfiguration house = new("Frysevarer", 25, configlist, configtime);
+        StorageConfiguration test = new("Frysevarer", 25, configlist, configtime);
 
         house.WareHouseConfigPrint();
         house.CreateStorage();
-        house.GetAllStorageInformationPrint();
-        Console.WriteLine("");
-        Console.WriteLine(house.PlacePackage(u));
-        Console.WriteLine(house.PlacePackage(u));
-        Console.WriteLine();
-        Console.WriteLine(house.FindPackageById(u.packageid));
-        Console.WriteLine();
-        Console.WriteLine(house.FindPackageSectionById(u.packageid));
-        Console.WriteLine();
-        Console.WriteLine(house.IsSpotTaken("FrysevarerShelfID: 1"));
-        Console.WriteLine(house.IsSpotTaken(house.GetStorageNameById(1)));
-        Console.WriteLine(house.GetStorageNameById(1));
-        Console.WriteLine(house.FindPackageById(u.packageid));
-        Console.WriteLine(house.FindPackageSectionById(u.packageid));
-        house.GetAllStorageInformationPrint();
-        Console.WriteLine(house.GetTimeStorageToTerminalSeconds());
-
-        ReceivingDepartment receivingDept = new ReceivingDepartment(house);
+       
+        ReceivingDepartment receivingDept = new(house);
 
         receivingDept.ReceivePackage(u);
+        receivingDept.ReceivePackage(uu);
+        receivingDept.printlistpackage();
+        receivingDept.SendFirstPackageToStorage();
+        house.GetAllStorageInformationPrint();
 
         List<string> placementResults = receivingDept.SendPackagesToWarehouse();
         foreach (var result in placementResults)
         {
             Console.WriteLine(result);
+            
         }
     }
 }
--- Ware/ReceivingDepartment.cs
+++ Ware/ReceivingDepartment.cs
@@ -4,32 +4,55 @@ using System.Collections.Generic;
 namespace Ware
 {
     /// <summary>
-    /// Her håndteres mottak av pakkene og tidene det tar.
+    /// The reception of the packages and the times it takes are handled here.
     /// </summary>
-    public class ReceivingDepartment
+    public class ReceivingDepartment(StorageConfiguration warehouse)
     {
-        private List<CreatePackage> receivedPackages;
-        private StorageConfiguration storageConfiguration;
-
-        public ReceivingDepartment(StorageConfiguration warehouse)
-        {
-            storageConfiguration = warehouse;
-            receivedPackages = new List<CreatePackage>();
-        }
+        private List<CreatePackage> receivedPackages = [];
+        private StorageConfiguration storageConfiguration = warehouse;
 
         /// <summary>
-        /// Pakke mottas og legges in i listen til mottate pakker.
+        /// The package is received and added to the list of received packages.
         /// </summary>
-        /// <param name="package">Navnet på pakken som mottas.</param>
+        /// <param name="package">The name of the package being received.</param>
         public void ReceivePackage(CreatePackage package)
         {
             receivedPackages.Add(package);
         }
 
+        public void SendFirstPackageToStorage()
+        {
+            storageConfiguration.PlacePackage(receivedPackages[0]);
+        }
+        public void SendAllPackagesToStorage()
+        {
+            foreach (var package in receivedPackages)
+            {
+                storageConfiguration.PlacePackage(package);
+                storageConfiguration.GetAllStorageInformationPrint();
+            }
+        }
+        public void printlistpackage() 
+        {
+            foreach (var package in receivedPackages) 
+            { 
+                Console.WriteLine(package);
+            }
+
+        }
+
+        public string TravelTimeToStorage() 
+        {
+            string time = "Estimated time to Storage: ";
+            return time+= storageConfiguration.GetTimeDeliveryToStorage();
+
+
+        }
+
         /// <summary>
-        /// Pakkene sendes til lagerhuset og returnerer en liste til pakkenes resultater.
+        /// The packages are sent to the warehouse and return a list of the packages' results.
         /// </summary>
-        /// <returns>Liste med strenger som hviser resultatet til plassering av hver pakke i lagerhuset.</returns>
+        /// <returns>List of strings that assign the result to the location of each package in the warehouse.</returns>
         public List<string> SendPackagesToWarehouse()
         {
             List<string> results = new List<string>();
@@ -38,8 +61,8 @@ namespace Ware
                 string result = storageConfiguration.PlacePackage(package);
                 int timeDeliveryToStorage = storageConfiguration.GetTimeDeliveryToStorage();
                 int timeStorageToTerminal = storageConfiguration.GetTimeStorageToTerminal();
-                result += $" Tid fra mottak til lager: {timeDeliveryToStorage} minutter.";
-                result += $" Tid fra lager til terminal: {timeStorageToTerminal} minutter.";
+                result += $" Time from receipt to storage: {timeDeliveryToStorage} minutes.";
+                result += $" Time from warehouse to terminal: {timeStorageToTerminal} minutes.";
                 results.Add(result);
             }
 
