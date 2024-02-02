e58d10a20ceef59c29c330ce58ae0e6b5591521a
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-02-02 18:06:44 +0100

    Added xml kommentar

--- Ware/ReceivingDepartment.cs
+++ Ware/ReceivingDepartment.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace Ware
 {
+    /// <summary>
+    /// Her håndteres mottak av pakkene og tidene det tar.
+    /// </summary>
     public class ReceivingDepartment
     {
         private List<CreatePackage> receivedPackages;
@@ -14,11 +17,19 @@ namespace Ware
             receivedPackages = new List<CreatePackage>();
         }
 
+        /// <summary>
+        /// Pakke mottas og legges in i listen til mottate pakker.
+        /// </summary>
+        /// <param name="package">Navnet på pakken som mottas.</param>
         public void ReceivePackage(CreatePackage package)
         {
             receivedPackages.Add(package);
         }
 
+        /// <summary>
+        /// Pakkene sendes til lagerhuset og returnerer en liste til pakkenes resultater.
+        /// </summary>
+        /// <returns>Liste med strenger som hviser resultatet til plassering av hver pakke i lagerhuset.</returns>
         public List<string> SendPackagesToWarehouse()
         {
             List<string> results = new List<string>();
