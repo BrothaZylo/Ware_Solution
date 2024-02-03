8161332a06173533af2df970d983558befa2aa24
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2024-02-03 15:40:59 +0100

    Fixed delvierySchedule and changed out all datatype "var" with correct types

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,11 +12,11 @@ PackageHistory packageHistory = new PackageHistory(new Dictionary<CreatePackage,
 CreatePackage iskrem = new CreatePackage("Iskrem", "kjølevare", "fast",43,4);
 CreatePackage flammekaster = new CreatePackage("Flammekaster", "Farlig gods", "fast", 89, 60);
 // Test for å legge til enkelte pakker
-/*
+
 packageHistory.DeliveryHistory(iskrem, DateTime.Now);
 packageHistory.DeliveryHistory(flammekaster, DateTime.Now);
 packageHistory.PickTime(iskrem, DateTime.Now.AddHours(10));
-*/ 
+
 
 //  Test for å legge til en liste med varer
 /*
@@ -30,24 +30,32 @@ packageHistory.SeveralPickup(packages, DateTime.Now.AddDays(4));
 
 //Hente infoen om alle varene 
 packageHistory.AllHistoryInfo();
-
+*/
 
 //Hente infoen om en enkelt vare
 packageHistory.OnePackageHistory(iskrem.packageid);
 
-*/
+
+/*
+DeliverySchedule deliverySchedule = new(new Dictionary<string, List<(string,CreatePackage, DateTime)>>());
+
+deliverySchedule.AddPackage("Monday",iskrem, DateTime.Now);
+deliverySchedule.AddPackage("Monday", u, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
+deliverySchedule.AddPackage("Monday", u, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
+deliverySchedule.AddPackage("Tuesday", flammekaster, DateTime.Now);
 
 
 
-List<DeliverySchedule.DeliveryList> packageDates =
-[
-    new() { Day = "Monday", Packages = iskrem, DeliveryTime = DateTime.Now, DeliveryType = 1},
-    new() { Day = "Tuesday", Packages = flammekaster, DeliveryTime = DateTime.Now, DeliveryType = 1 }
 
+deliverySchedule.SchedulePackages("Monday");
+deliverySchedule.SchedulePackages("Tuesday");
 
-];
+DeliverySchedule deliverySchedule = new DeliverySchedule();
 
-DeliverySchedule schedule = new DeliverySchedule(packageDates);
-
-schedule.CreateSchedule();
-schedule.SchedulePrint();
+deliverySchedule.AddPackageToDay(DayOfWeek.Monday, flammekaster, DateTime.Now);
+Console.WriteLine(deliverySchedule.GetCalender());*/
--- Ware/DeliverySchedule.cs
+++ Ware/DeliverySchedule.cs
@@ -2,52 +2,88 @@
 using System.Collections.Generic;
 using System.IO.IsolatedStorage;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Ware
-{
-    public class DeliverySchedule(List<DeliverySchedule.DeliveryList> ListOfDeliveries)
+{   
+    /// <summary>
+    /// A class that creates a calender schedule for delivery
+    /// </summary>
+    public class DeliverySchedule
     {
-        private Dictionary<string, List<DeliveryList>> Calender = [];
-        private List<DeliveryList> YourDeliveryList = ListOfDeliveries;
-        private List<DeliveryList> PackageList = new();
-
-        public void CreateSchedule()
+        /// <summary>
+        /// Creates an enum that will be used as keys for the calender/dictionary
+        /// </summary>
+        public enum DaysOfWeek
         {
-            int count = 1;
-            foreach (var i in ListOfDeliveries)
-            {
-                Calender.Add(i.Day,(PackageList));
-                count++;
-                
-            }
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
         }
 
-        public void SchedulePrint()
+        private Dictionary<DaysOfWeek, List<(CreatePackage, DateTime)>> calender;
+        
+        /// <summary>
+        /// When creating a DeliverySchedule will it run PrepareDictinary() that will create 
+        /// the calender with the 7 days as keys
+        /// </summary>
+        public DeliverySchedule()
         {
-            foreach(var i in Calender)
-            {
-                Console.WriteLine(i.Key);
-                foreach (var j in PackageList)
-                {
-                    Console.WriteLine(j.Day);
-                    if (i.Key == j.Day)
-                    {
-                        Console.WriteLine($"Id: {j.Packages.packageid} Name: {j.Packages.name} Type of goods: {j.Packages.goods}");
+            PrepareDictionary();
+        }
+        /// <summary>
+        /// When PrepareDictionary is called will it create a dictionary called calender and add the days monday-sunday
+        /// as keys inside the dictionary with an empty list as its value
+        /// https://stackoverflow.com/questions/26160503/creating-dictionaries-with-pre-defined-keys-c-sharp
+        // Author visc
+        /// </summary>
+        public void PrepareDictionary()
+        {
+            calender = new Dictionary<DaysOfWeek, List<(CreatePackage, DateTime)>>();
 
-                    }
+            foreach (DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+            {
+                calender.Add(day, new List<(CreatePackage, DateTime)>());
+            }
+            foreach (KeyValuePair<DaysOfWeek, List<(CreatePackage, DateTime)>> item in calender)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+        //https://stackoverflow.com/questions/14991688/adding-items-to-a-list-in-a-dictionary
+        //Author Steve
+        public void AddPackageToDay(DayOfWeek day, CreatePackage package,DateTime time)
+        {
+            if (calender.ContainsKey((DaysOfWeek)day))
+            {
+                calender[(DaysOfWeek)day].Add((package,time));
+            } 
+        }
+
+        public Dictionary<DaysOfWeek, List<(CreatePackage, DateTime)>> GetCalender()
+        {
+            foreach (KeyValuePair<DaysOfWeek, List<(CreatePackage, DateTime)>> keys in calender)
+            {
+                Console.WriteLine($"ID: {keys.Key}");
+                foreach ((CreatePackage, DateTime) items in keys.Value) 
+                {
+                    Console.WriteLine($"ID: {items}   " +
+                    $"      Name: {items.Item1.packageid}" +
+                    $"      Type: {items.Item1.name}" +
+                    $"      Speed: {items.Item1.goods}");
+
                 }
             }
+            return calender;
         }
 
-
-        public class DeliveryList 
-        {
-            public string Day;
-            public CreatePackage Packages;
-            public DateTime DeliveryTime;
-            public int DeliveryType;
-        }
     }
+
 }
