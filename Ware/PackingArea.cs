8bd83ff5a72ff500f39d33d06ebe7ad39315ea4d
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-04-10 17:33:14 +0200

    fixed PackingArea

--- Ware/PackingArea.cs
+++ Ware/PackingArea.cs
@@ -8,7 +8,13 @@ using Ware;
 public class PackingArea
 {
     private readonly List<Pallet> pallets = new List<Pallet>();
+    private readonly List<Package> unallocatedPackages = new List<Package>();
 
+    /// <summary>
+    /// Adds a package to a pallet. If the pallet is full a exception is thrown.
+    /// </summary>
+    /// <param name="package">The package to add.</param>
+    /// <param name="pallet">The pallet to add the package to.</param>
     public void AddToPallet(Package package, Pallet pallet)
     {
         if (!pallet.IsPalletFull())
@@ -22,17 +28,42 @@ public class PackingArea
         }
     }
 
+    /// <summary>
+    /// Receives a package and stores it in the packing area storage.
+    /// </summary>
+    /// <param name="package">The package to be stored.</param>
     public void ReceivePackage(Package package)
     {
-        Pallet? targetPallet = pallets.FindLast(p => !p.IsPalletFull());
+        unallocatedPackages.Add(package);
+    }
 
-        if (targetPallet == null)
+    /// <summary>
+    /// Allocates stored packages to pallets. Creates new pallets if necessary.
+    /// </summary>
+    public void AllocatePackagesToPallets()
+    {
+        foreach (Package package in unallocatedPackages)
         {
-            targetPallet = new Pallet();
-            pallets.Add(targetPallet);
+            Pallet? targetPallet = null;
+            for (int i = pallets.Count - 1; i >= 0; i--)
+            {
+                if (!pallets[i].IsPalletFull())
+                {
+                    targetPallet = pallets[i];
+                    break;
+                }
+            }
+
+            if (targetPallet == null)
+            {
+                targetPallet = new Pallet();
+                pallets.Add(targetPallet);
+            }
+
+            targetPallet.AddPackageToPallet(package);
         }
 
-        targetPallet.AddPackageToPallet(package);
+        unallocatedPackages.Clear();
     }
 
     public IReadOnlyList<Pallet> Pallets
@@ -46,5 +77,4 @@ public class PackingArea
     {
         PackageAddedToPallet?.Invoke(this, new PackageEventArgs(package));
     }
-
 }
