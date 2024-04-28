7d1e4c5c379bb799ae8d2bd8961bf7178d7baed1
BrothaZylo <83866259+BrothaZylo@users.noreply.github.com>
2024-04-28 19:30:01 +0200

    Fixes for gui sim

--- Ware/Simulation.cs
+++ Ware/Simulation.cs
@@ -6,6 +6,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Ware
 {
