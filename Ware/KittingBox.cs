32614850e3e667d85ade98135304bdeb4bf6f2aa
JohnDrybones <johannes.j.moulazimis@hiof.no>
2024-05-26 13:55:40 +0200

    Made kittingBox class to inherit from package

---
+++ Ware/KittingBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ware.Packages;
+
+namespace Ware
+{
+    public class KittingBox : Package
+    {
+
+        public KittingBox(string packageName = "Undefined", string goodsType = "Undefined", double packageHeightCm = 1, double packageWidthCm = 1)
+        {
+
+        }
+    }
+}
